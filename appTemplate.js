82bea7c64ff181a4c2372ccb28ed880a3ac53773
duder <pmulc07@gmail.com>
2013-12-15 18:05:23 -0500

    switched to using name app.js instead of server.js
    Committed a template version instead and updated gitignore so that we
    don't commit our app.js with the credentials in it

--- appTemplate.js
+++ appTemplate.js
@@ -1,19 +1,25 @@
 (function () {
+      // import core modules
   var PlugAPI = require('./lib/plugapi'),
       PlugBot = require('./plugBot').Bot,
+      
+      // import bot plugin modules
       Skitty = require('./modules/skitty').Skitty,
       PlaylistManager = require ('./modules/playlistManager').PlaylistManager;
 
+      // define bot model
   var model = {
         room: 'coding-soundtrack-lounge',
         updateCode: '$&2h72=^^@jdBf_n!`-38UHs', 
         auth: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
       },
+
+      // create plugins
       plugins = [ 
         new Skitty(),
         new PlaylistManager({ defaultPlaylist: 'snags' })
       ];
 
-      
+      // create bot
   var skitty = new PlugBot(model, PlugAPI, plugins);
 }());
