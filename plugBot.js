afcf320cf8974f014965e175c07294d8464d582f
Hect1c <hect1c17@gmail.com>
2014-07-22 16:52:55 -0400

    Removed getUpdateCode method from fbot skeleton.

--- plugBot.js
+++ plugBot.js
@@ -9,47 +9,41 @@
 
       self.reconnectAttempts = 0;
 
-      PlugApi.getUpdateCode( function(error, updateCode) {
-        if(error === false) {
-          var api = new PlugApi(model.auth, updateCode),
-              core = new PlugCore(self, api);
+      var api = new PlugApi(model.auth),
+          core = new PlugCore(self, api);
 
-          // register plugins
-          for (var i = 0; i < plugins.length; i++) {
-            plugins[i].init(api, core);
+      // register plugins
+      for (var i = 0; i < plugins.length; i++) {
+        plugins[i].init(api, core);
+      }
+
+      // <auto-reconnect>
+        var delay = model.reconnectDelay || 0;
+
+        model.reconnectAttempts = model.reconnectAttempts || 100;
+
+        var reconnect = function() {
+          var err = arguments;
+          if (self.reconnectAttempts < model.reconnectAttempts) {
+            setTimeout(function () {
+              console.log('Disconnected.');
+              console.log(err);
+              api.connect(model.room);
+            }, delay);
+
+            self.reconnectAttempts++;
           }
+        };
 
-          // <auto-reconnect>
-            var delay = model.reconnectDelay || 0;
+        api.on('close', reconnect);
+        api.on('error', reconnect);
+      // </auto-reconnect>
 
-            model.reconnectAttempts = model.reconnectAttempts || 100;
-
-            var reconnect = function() {
-              var err = arguments;
-              if (self.reconnectAttempts < model.reconnectAttempts) {
-                setTimeout(function () {
-                  console.log('Disconnected.');
-                  console.log(err);
-                  api.connect(model.room);
-                }, delay);
-
-                self.reconnectAttempts++;
-              }
-            };
-
-            api.on('close', reconnect);
-            api.on('error', reconnect);
-          // </auto-reconnect>
-
-          // connect
-          api.on('roomJoin', function(data) {
-            console.log('Connected to: ' + data.room.name);
-          });
-          api.connect(model.room);
-        } else {
-          console.log(error);
-        }
+      // connect
+      api.on('roomJoin', function(data) {
+        console.log('Connected to: ' + data.room.name);
       });
+      api.connect(model.room);
     }
   // </skeleton plugbot>
 
