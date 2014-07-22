79feaa62395bea4bb58f389da6595fde3e39dad4
Hect1c <hect1c17@gmail.com>
2014-07-22 19:47:26 -0400

    Fixed typo...smh

--- appTemplate.js
+++ appTemplate.js
@@ -99,7 +99,7 @@
   var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
   var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
   app.listen( port, ipaddress, function() {
-      console.log(('[Node Server] Connected');
+      console.log('[Node Server] Connected');
   });
 
 }());
