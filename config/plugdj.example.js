d1f7c3a4a5b1866585cb795336e629812aeaaf94
Hartley <hartley@dc-professional.com>
2014-09-16 12:53:04 -0400

    Updated auth object based on new plugapi.

--- config/plugdj.example.js
+++ config/plugdj.example.js
@@ -1,4 +1,7 @@
 module.exports = {
-	room : 'plugdj-room-name',
-	auth : 'usr-cookie-value'
-};
+    room: 'plugdj-room-name',
+    auth: {
+        email : 'account-email',
+        password : 'account-password'
+    }
+}
