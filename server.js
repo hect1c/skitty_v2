e3df43381f3b55221a6f8072dd30b077d917d4b3
Eran <me@eran.sh>
2014-11-15 20:13:45 -0600

    Remove MEAN stack

--- server.js
+++ server.js
@@ -15,20 +15,5 @@ var init = require('./config/init')(),
 // Bootstrap db connection
 var db = mongoose.connect(config.db);
 
-// Init the express application
-var app = require('./config/express')(db);
-
-// Bootstrap passport config
-require('./config/passport')();
-
-// Start the app by listening on <port>
-app.listen(config.port);
-
-// Expose app
-exports = module.exports = app;
-
 // Launch bot
 var skitty = require('./app/bot')(config, db);
-
-// Logging initialization
-console.log('MEAN.JS application started on port ' + config.port);
