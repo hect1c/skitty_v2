3398df81fa37a19207edb8c6d7f50e0ded9fc8b7
Hartley <hartley@dc-professional.com>
2014-12-29 18:21:35 -0500

    Added final package to skitty skeleton.

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 //load app settings
 var config = SkittyConfig.load(),
+    db = SkittyDb,
     //load modules
     modules = [
-        new Skitty(),
-        new SkittyCurmudgeon(),
+        new Skitty,
+        new SkittyCurmudgeon,
         new SkittyInfo,
-        new SkittyKillSwitch
+        new SkittyKillSwitch,
+        new SkittyStats,
+        new SkittyDj
     ],
     //run app
     skitty = SkittyPlugApi.run(config, modules);
